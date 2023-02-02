d63d17395059f3c8cdc5748e91041a897439889d
Dwain06 <dwain.mobile@gmail.com>
2023-02-02 15:39:57 +0100

    add(component): header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import "./header.css"
+
+import logo from "../../utils/logo.jpg"
+
+const Header = () => {
+    return (
+        <>
+            <div className="header">
+                <div className="header--container">
+                    <NavLink className="header__logo" to="/">
+                        <img src={logo} alt="Logo" />
+                    </NavLink>
+                    <div className="header__title">
+                        HRnet application
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Header;
