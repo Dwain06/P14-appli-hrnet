058f4c4499407786681ea0a52ec8c404fab00c00
Dwain06 <dwain.mobile@gmail.com>
2023-02-13 17:41:56 +0100

    add(employees page) delete employee

--- src/context/employeesReducer.js
+++ src/context/employeesReducer.js
@@ -1,13 +1,23 @@
 const employeesReducer = (state, action) => {
     switch (action.type) {
         case "SET_EMPLOYEES":
-            return state = {employeesList: action.payload};
+            return (state = { employeesList: action.payload });
         case "NEW_EMPLOYEE":
-            return state = {employeesList: [...state.employeesList, action.payload]};
+            return (state = {
+                employeesList: [...state.employeesList, action.payload],
+            });
+        case "DELETE_EMPLOYEE":
+            const arr = [...state.employeesList];
+            const idToDelete = action.payload;
+            const objWithIdIndex = arr.findIndex((obj) => obj.id === idToDelete);
+            if (objWithIdIndex > -1) {
+                arr.splice(objWithIdIndex, 1);
+            }
+            return (state = { employeesList: [...arr] });
 
         default:
             return state;
-      }
-  };
-  
-  export default employeesReducer;
+    }
+};
+
+export default employeesReducer;
--- src/pages/employees/Employees.jsx
+++ src/pages/employees/Employees.jsx
@@ -8,17 +8,9 @@ import MainNav from "../../components/mainNav/MainNav";
 import { EmployeesContext } from "../../context/employeesContext";
 
 // Import packages
-import { DataGrid } from "@mui/x-data-grid";
 import MaterialTable from "@material-table/core";
 
 const columns = [
-    // { headerName: "Pic", field: "pic", width: 50, renderCell: (params) => {
-    //     return (
-    //         <div className="avatar--container">
-    //             <span className="avatar">{params.row.lastName[0].toUpperCase()}</span>
-    //         </div>
-    //     )
-    // } },
     { title: "First Name", field: "firstName" },
     { title: "Last Name", field: "lastName" },
     { title: "Birth date", field: "dateOfBirth" },
@@ -30,51 +22,12 @@ const columns = [
     { title: "Department", field: "department" },
 ];
 
-// const actionColumn = [
-//     {
-//         field: "action",
-//         headerName: "Action",
-//         renderCell: () => {
-//             return (
-//                 <div className="cellAction">
-//                     <div className="deleteButton" onClick={(e) => deleteEmployee(e)}>Delete</div>
-//                 </div>
-//             )
-//         }
-//     }
-// ]
-
 const Login = () => {
-
     const { dispatch, employeesList } = useContext(EmployeesContext);
 
     useEffect(() => {
         document.title = "HRnet - Current Employees";
     }, []);
-
-    const actionColumn = [
-        {
-            field: "action",
-            headerName: "Action",
-            renderCell: (params) => {
-                return (
-                    <div className="cellAction">
-                        <div className="deleteButton" onClick={() => deleteEmployee(params.row.id)}>Delete</div>
-                    </div>
-                )
-            }
-        }
-    ]
-
-    const deleteEmployee = (id) => {
-        const arr = [...employeesList]
-        const objWithIdIndex = arr.findIndex((obj) => obj.id === id);
-        console.log(objWithIdIndex)
-        if (objWithIdIndex > -1) {
-            arr.splice(objWithIdIndex, 1);
-        }
-        console.log(arr);
-    }
 
     return (
         <>
@@ -83,19 +36,25 @@ const Login = () => {
             <div className="employees-list">
                 <div className="employees-list--container">
                     <div className="employees-list--table">
-                        {/* <div style={{ height: 400, width: "100%" }}>
-                            <DataGrid
-                                rows={employeesList}
-                                columns={columns.concat(actionColumn)}
-                                pageSize={5}
-                                rowsPerPageOptions={[5]}
-                                checkboxSelection = {false}
-                            />
-                        </div> */}
-                        <MaterialTable 
-                            columns={columns} 
+                        <MaterialTable
+                            columns={columns}
                             data={employeesList}
                             title="Employees list"
+                            options={{
+                                actionsColumnIndex: -1,
+                            }}
+                            editable={{
+                                onRowDelete: (rowData) =>
+                                    new Promise((resolve, reject) => {
+                                        setTimeout(() => {
+                                            dispatch({
+                                                type: "DELETE_EMPLOYEE",
+                                                payload: rowData.id,
+                                            });
+                                            resolve();
+                                        }, 1000);
+                                    }),
+                            }}
                         />
                     </div>
                 </div>
